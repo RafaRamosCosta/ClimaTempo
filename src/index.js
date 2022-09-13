1f4039fbfd0dee17e648be540d7072b7288604fa
RafaRamosCosta <rm94281@fiap.com.br>
2022-09-13 17:29:40 -0300

    Estrutura da aplicação iniciada.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ClimaTempo from './components/climaTempo/ClimaTempo';
+
+ReactDOM.render(<ClimaTempo />, document.getElementById('root'));
